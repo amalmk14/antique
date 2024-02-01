a357deb610ee8fcc1800011ca8dbac8cc46d6f38
amalmk14 <amalmk2k16@gmail.com>
2024-02-01 12:04:01 +0530

    set image urls and views

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from . import views
 
-app_name = 'antique'
+app_name = 'app'
 urlpatterns = [
     path('',views.home,name='home'),
     path('admin/',views.admin,name='admin'),
+    path('mainimg/',views.mainimg,name='mainimg'),
+    path('menumainimg/',views.menumainimg,name='menumainimg'),
+    path('menuimg1/',views.menuimg1,name='menuimg1'),
+    path('menuimg2/',views.menuimg2,name='menuimg2'),
+    path('menuimg3/',views.menuimg3,name='menuimg3'),
+    path('menuimg4/',views.menuimg4,name='menuimg4'),
+    path('menuimg5/',views.menuimg5,name='menuimg5'),
+    path('menuimg6/',views.menuimg6,name='menuimg6'),
+    path('menuimg7/',views.menuimg7,name='menuimg7'),
+    path('menuimg8/',views.menuimg8,name='menuimg8'),
+    path('aboutimg/',views.aboutimg,name='aboutimg'),
+    path('contactimg/',views.contactimg,name='contactimg'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 # Create your views here.
 
@@ -70,6 +70,104 @@ def admin(request):
                                      menu_img2_1=menu_img2_1,menu_img2_2=menu_img2_2,menu_img2_3=menu_img2_3,menu_img3_1=menu_img3_1,menu_img3_2=menu_img3_2,menu_img3_3=menu_img3_3,
                                      menu_img4_1=menu_img4_1,menu_img4_2=menu_img4_2,menu_img4_3=menu_img4_3,menu_img5_1=menu_img5_1,menu_img5_2=menu_img5_2,menu_img5_3=menu_img5_3,
                                      menu_img6_1=menu_img6_1,menu_img6_2=menu_img6_2,menu_img6_3=menu_img6_3,menu_img7_1=menu_img7_1,menu_img7_2=menu_img7_2,menu_img7_3=menu_img7_3,
-                                     menu_img8_1=menu_img8_1,menu_img8_2=menu_img8_2,menu_img8_3=menu_img8_3)
+                                     menu_img8_1=menu_img8_1,menu_img8_2=menu_img8_2,menu_img8_3=menu_img8_3,contact1=contact1,contact2=contact2,contact3=contact3,contact4=contact4,
+                                     contact5=contact5,contact6=contact6,contact7=contact7,contact8=contact8,contact9=contact9,
+                                     footer1=footer1,footer2=footer2,footer3=footer3)
         
     return render(request,'basictable.html',{'antique':antique})
+
+def mainimg(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('main_img')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+
+def menumainimg(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_mainimg')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg1(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img1')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg2(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img2')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg3(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img3')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg4(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img4')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg5(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img5')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg6(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img6')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg7(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img7')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def menuimg8(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('menu_img8')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def aboutimg(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('abou_img')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
+    
+def contactimg(request):
+    if request.method == 'POST':
+        about_img = request.FILES.get('contact_img')
+        antique_instance = Antique.objects.first()  # You may want to retrieve the specific instance you want to update
+        antique_instance.about_img = about_img
+        antique_instance.save()
+        return redirect('app:admin')
